3510b9dc68651200e53b2e64f0539f336c549753
Bilal Hussain <hussain.bilal000786@gmail.com>
2021-06-05 20:56:07 +0400

    refactored code

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -8,8 +8,8 @@ const blockName = 'navigation';
 export default function Navigation() {
   return (
     <div className={`${blockName}__container`}>
-      <Link className={`${blockName}__nav`} to="/"><MdHome /></Link>
-      <Link className={`${blockName}__nav`} to="/liked"><MdFavorite /></Link>
+      <Link className={`${blockName}__nav`} to="/"><MdHome /><h3>Home</h3></Link>
+      <Link className={`${blockName}__nav`} to="/liked"><MdFavorite /><h3>Liked</h3></Link>
     </div>
   )
 }
