8786eae048fac36ab36ddf33b3c3fc09868f1985
Bilal Hussain <hussain.bilal000786@gmail.com>
2021-06-05 21:32:24 +0400

    refactored code

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,19 +5,22 @@ import Home from './Home';
 import Liked from './Liked';
 import Navigation from './Navigation';
 
+const blockName = 'app';
 class App extends Component {
   render() {
     return (
         <Router>
-          <Navigation />
-          <Switch>
-            <Route path="/liked">
-              <Liked />
-            </Route>
-            <Route exact path="/">
-              <Home />
-            </Route>
-          </Switch>
+          <div className={`${blockName}__container`}>
+            <Navigation />
+            <Switch>
+              <Route path="/liked">
+                <Liked />
+              </Route>
+              <Route exact path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>        
         </Router>
     );
   }
